feac8610608a86c09312e54fadab9747f84008a4
arthur <arthur.mengual@gmail.com>
2022-02-09 11:25:44 -0300

    test in progress

--- tests/unit/test_puchase.py
+++ tests/unit/test_puchase.py
@@ -1,9 +1,12 @@
 import pytest
+import server
 
 
 class Testpurchase:
-    def more_thant_club_points_should_return_error_message(self):
-        pass
+    def more_thant_club_points_should_return_error_message(self, client, clubs_fixture, mocker):
+        club_test = clubs_fixture['clubs'][0]
+        mocker.patch.object(server.purchasePlaces, 'club', club_test)
+        data = {'places': int(club_test['ponts']+1)}
+        response = client.post('/purchasePlaces', data=data)
 
-    def less_or_equal_to_club_points_should_return_200(self):
-        pass
+        assert b'sorry, you do not have enough points' in response.data
