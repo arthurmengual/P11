fbbcea416e064bcfe66009b8e9d0b6a56e225518
arthur <arthur.mengual@gmail.com>
2022-02-09 17:23:38 -0300

    initial commit bug 3

---
+++ tests/unit/test_purchase.py
@@ -0,0 +1,16 @@
+import pytest
+import server
+
+
+class Testpurchase:
+    def test_more_than_12_points_shoul_return_error_message(self, client, clubs_fixture, competitions_fixture, mocker):
+        mocker.patch.object(server, 'clubs', clubs_fixture['clubs'])
+        mocker.patch.object(server, 'competitions',
+                            competitions_fixture['competitions'])
+        club = clubs_fixture['clubs'][0]
+        competition = clubs_fixture['competitions'][0]
+        places = 13
+        data = {'club': club, 'competition': competition, 'places': places}
+        response = client.post('/purchasePlaces', data=data)
+
+        assert b'sorry, you cannot purchase more that 12 places' in response.data
