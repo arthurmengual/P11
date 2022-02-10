503da3df6713a03b6c1881b91d8157797743c9e2
arthur <arthur.mengual@gmail.com>
2022-02-10 09:59:20 -0300

    fixed bug3 and tests

--- tests/unit/test_purchase.py
+++ tests/unit/test_purchase.py
@@ -3,7 +3,7 @@ import server
 
 
 class Testpurchase:
-    def test_more_than_12_points_shoul_return_error_message(self, client, clubs_fixture, competitions_fixture, mocker):
+    def test_more_than_12_points_should_return_error_message(self, client, clubs_fixture, competitions_fixture, mocker):
         mocker.patch.object(server, 'clubs', clubs_fixture['clubs'])
         mocker.patch.object(server, 'competitions',
                             competitions_fixture['competitions'])
@@ -12,5 +12,32 @@ class Testpurchase:
         places = 13
         data = {'club': club, 'competition': competition, 'places': places}
         response = client.post('/purchasePlaces', data=data)
+        print(response.data)
 
-        assert b'sorry, you cannot purchase more that 12 places' in response.data
+        assert b'sorry, you cannot purchase more than 12 places' in response.data
+
+    def test_12_points_should_return_200(self, client, clubs_fixture, competitions_fixture, mocker):
+        mocker.patch.object(server, 'clubs', clubs_fixture['clubs'])
+        mocker.patch.object(server, 'competitions',
+                            competitions_fixture['competitions'])
+        club = clubs_fixture['clubs'][0]
+        competition = competitions_fixture['competitions'][0]
+        places = 12
+        data = {'club': club, 'competition': competition, 'places': places}
+        response = client.post('/purchasePlaces', data=data)
+
+        assert b'sorry, you cannot purchase more than 12 places' not in response.data
+        assert response.status_code == 200
+
+    def test_12_points_should_return_200(self, client, clubs_fixture, competitions_fixture, mocker):
+        mocker.patch.object(server, 'clubs', clubs_fixture['clubs'])
+        mocker.patch.object(server, 'competitions',
+                            competitions_fixture['competitions'])
+        club = clubs_fixture['clubs'][0]
+        competition = competitions_fixture['competitions'][0]
+        places = 11
+        data = {'club': club, 'competition': competition, 'places': places}
+        response = client.post('/purchasePlaces', data=data)
+
+        assert b'sorry, you cannot purchase more than 12 places' not in response.data
+        assert response.status_code == 200
