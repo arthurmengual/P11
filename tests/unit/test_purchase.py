9ad757709465e25c575fa0faf642ce89c0dab7f5
arthur <arthur.mengual@gmail.com>
2022-02-15 19:14:48 -0300

    added context manager for board

--- tests/unit/test_purchase.py
+++ tests/unit/test_purchase.py
@@ -1,5 +1,20 @@
-import pytest
 import server
+from server import app
+from flask import template_rendered
+from contextlib import contextmanager
+
+
+@contextmanager
+def captured_templates(app):
+    recorded = []
+
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
 
 
 class Testpurchase:
